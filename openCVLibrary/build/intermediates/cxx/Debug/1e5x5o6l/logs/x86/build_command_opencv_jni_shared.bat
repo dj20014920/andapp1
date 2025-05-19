e67592ab747bcfd4e19e42396e6a26e7714daac6
황원 <ghkddnjs1203@naver.com>
2025-05-19 14:12:50 +0900

    영수증 분석 기능(정확히는 인식 못함2)

---
+++ openCVLibrary/build/intermediates/cxx/Debug/1e5x5o6l/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\ghkdd\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\ghkdd\\AndroidStudioProjects\\andapp1\\openCVLibrary\\.cxx\\Debug\\1e5x5o6l\\x86" ^
+  opencv_jni_shared
